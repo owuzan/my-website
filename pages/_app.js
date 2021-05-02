f601e55d49dfd9d2c3c6a6a5209eee6c61285e2f
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2021-05-03 00:10:58 +0300

    Added "Inter" font

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import "tailwindcss/tailwind.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col space-y-12 bg-gray-50">
-      <Header />
-      <div className="flex-auto">
-        <Component {...pageProps} />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="min-h-screen flex flex-col space-y-12 bg-gray-50">
+        <Header />
+        <div className="flex-auto">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
